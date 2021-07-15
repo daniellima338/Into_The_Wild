17c3a7a2341c7e64cdd0b0523d8c00613cfdf568
daniellima338 <daniellima338@gmail.com>
2021-07-15 14:28:37 +0000

    added animation to the counter. redirect to game when countdown ends

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,6 +1,23 @@
-var seconds = document.getElementById("countdown").textContent;
-var countdown = setInterval(function() {
-    seconds--;
-    document.getElementById("countdown").textContent = seconds;
-    if (seconds <= 0) window.location.href= "/play.html"
-}, 1000);
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.center');
+
+runAnimation();
+
+function runAnimation() {
+	nums.forEach((num, idx) => {
+		const penultimate = nums.length - 1;
+		num.addEventListener('animationend', (e) => {
+			if(e.animationName === 'goIn' && idx !== penultimate){
+				num.classList.remove('in');
+				num.classList.add('out');
+			} else if (e.animationName === 'goOut' && num.nextElementSibling){
+				num.nextElementSibling.classList.add('in');
+			} else {
+				counter.classList.add('hide');
+                window.location.href="/play.html";
+			}
+		});
+	});
+}
+
+
