ad9e55779a3d9ab752db8519d0bfccab79292e7e
daniellima338 <daniellima338@gmail.com>
2021-07-21 09:02:18 +0000

    work on increment score function. WIP

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,45 +4,44 @@ $.ajax({
     url: 'https://opentdb.com/api.php?amount=20&category=27&type=multiple',
     data: "json",
     success: function(response){
-        showQuestion(response.results);
+        setupQuiz(response.results);
     }
 });
 
-// Used to set up a new question everytime a answer is clicked
-function showQuestion(data) {
-    var current_question_id = 0; 
-    setupAnswers(data[current_question_id])
 
-    $("button").click(function(){
-        current_question_id++;
-        setupAnswers(data[current_question_id])
-    })
+// Used to generate random answer. Taken from stackoverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Used to recycle the correct answer everytime. CANT GET THE ANSWERS TO DISPLAY
-function setupAnswers(results) {
-    let isCorrect = results.correct_answer;
-    let incorrect_answers = results.incorrect_answers;
+// Used to generate new question everytime an answer is clicked
+function setupQuiz(data){
 
-    $("#question").text(results.question)
-    // sort((a,b)) => 0.5 - Math.random()) is used to always choose a random place to put the correct answer. Solution found on StackOverflow
-    
-    answers = [results.correct_answer]
-        .concat(results.incorrect_answers)
-        .sort((a, b) => 0.5 - Math.random());
+    var current_question_id = 0;
+    setupAnswers(data[current_question_id])
+
+    $("button").click(function() {
+        current_question_id++
+        setupAnswers(data[current_question_id])
+    });
+    console.log(data)            
+}
+
+function setupAnswers(current){
+   $("#question").text(current.question)
+
+    answers = [current.correct_answer]
+                    .concat(current.incorrect_answers)
+                    .sort((a, b) => 0.5 - Math.random());
     
     var i = 1;
     answers.forEach(function (answer) {
-    $("#answer_" + i).text(answer)
-    i++;
-    }) 
-    if (isCorrect) {
-        $("#score").innertext = $("#score") + 1;
-    }
-    else if (incorrect_answers) {
-        $("#incorrect").innertext = $("#incorrect") + 1;
-    }
-};
-
-
+        $("#answer_" + i).text(answer)  
+        i++
+    })
+     
+}
 
