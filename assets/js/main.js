c05898b4897a08aabcdec7d73f6591d9c4f375e7
daniellima338 <daniellima338@gmail.com>
2021-07-24 09:27:09 +0000

    changed from using 2 modals to one, by simplifying with js code. beautified the code for better readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 // GAME
 
 // Used to get the data
-
 $.ajax({
     type: "GET",
     url: 'https://opentdb.com/api.php?amount=20&category=27&type=multiple',
     data: "json",
-    success: function(response){
+    success: function(response) {
         setupQuiz(response.results);
     }
 });
@@ -29,7 +28,7 @@ function getRandomInt(min, max) {
 
 // Used to set up the quiz
 
-function setupQuiz(data){
+function setupQuiz(data) {
     quizData = data
     setupAnswers(quizData[currentQuestionId])
     console.log(quizData)
@@ -40,53 +39,55 @@ function setupQuiz(data){
 
 function triggerNextQuestion() {
     var delayTrigger = 2000;
-        setTimeout(function() {
-            currentQuestionId++
-                setupAnswers(quizData[currentQuestionId])
-        }, delayTrigger);
+    setTimeout(function() {
+        currentQuestionId++
+        setupAnswers(quizData[currentQuestionId])
+    }, delayTrigger);
 }
 
 //Used to generate the answers for each question
 
-function setupAnswers(current){
+function setupAnswers(current) {
     $("#question").html(current.question)
- 
-     answers = [current.correct_answer]
-                     .concat(current.incorrect_answers)
-                     .sort((_a, _b) => 0.5 - Math.random());
-     
-     var i = 1;
-     answers.forEach(function (answer) {
-         var answerButton = $("#answer_" + i)
-         answerButton.removeAttr('style');
-         answerButton.html(answer)  
-         i++
-     })
-     showCorrectAnswer(current.correct_answer) 
- }
 
-// Stores the scores after a game if finished
-function storeScores (newScore, playername) {
-            var highScores = JSON.parse(localStorage.getItem('highScores')) || []
-            var score = {
-                    score: newScore, 
-                    name: playername
-                };  
-                highScores.push(score);
-                highScores.sort((a, b) => b.score - a.score);
-                highScores.splice(5);
-                localStorage.setItem('highScores', JSON.stringify(highScores));
-                console.log(highScores)
+    answers = [current.correct_answer]
+        .concat(current.incorrect_answers)
+        .sort((_a, _b) => 0.5 - Math.random());
+
+    var i = 1;
+    answers.forEach(function(answer) {
+        var answerButton = $("#answer_" + i)
+        answerButton.removeAttr('style');
+        answerButton.html(answer)
+        i++
+    })
+    showCorrectAnswer(current.correct_answer)
+}
+
+// Stores the scores after a game if finished. Inspiration taken from: 
+//https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
+
+function storeScores(newScore, playername) {
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+    var score = {
+        score: newScore,
+        name: playername
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    console.log(highScores)
 };
 
 // Displays modal for the user
 function modalDisplay(modalName) {
-$("#finalScore").text("Your score was" + score)
-            $(modalName).modal("toggle")
-            $(".saveScore").click(function() {
-                storeScores(score, $("#name").val())
-            })
-        }
+    $("#finalMessage").text("Your score was" + score)
+    $(modalName).modal("toggle")
+    $(".saveScore").click(function() {
+        storeScores(score, $("#name").val())
+    })
+}
 
 function showCorrectAnswer(correct_answer) {
     $("button").click(function() {
@@ -103,32 +104,34 @@ function showCorrectAnswer(correct_answer) {
         $("button").unbind("click"); //unbind is used to reset the color
         $("#progressbar > div").width(life + "%")
 
-        //losing the game case. Inspiration taken from: https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
-        
-        if (life == 0){
+        if (life == 75) {
             modalDisplay("#endModal")
-            } else {
+            $("#titleText").text("You died in the jungle!")
+        } else {
             triggerNextQuestion()
         }
     });
-    if (score == 10) {
-        modalDisplay("#winModal")
-        }
+    if (score == 2) {
+        modalDisplay("#endModal")
+        $("#titleText").text("You got through the jungle. Well done!")
     }
-    
+}
+
 //Show highscores on the highscores board. Inspiration taken from: 
 //https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
 
 highScores = JSON.parse(localStorage.getItem('highScores')) || []
 console.log(highScores)
 $("#highScoresList").html(highScores
-    .map(score => {return (`<li class = "high-score">${score.name}  -  ${score.score}<li>`);})
+    .map(score => {
+        return (`<li class = "high-score">${score.name}  -  ${score.score}<li>`);
+    })
     .join(""))
 
 //EMAIL WORKFLOW. Taken from Code Institute course
 
 function sendMail(contactForm) {
-    emailjs.send("service_imbql8k","intoTheWild", {
+    emailjs.send("service_imbql8k", "intoTheWild", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.feedback.value
@@ -137,15 +140,7 @@ function sendMail(contactForm) {
             console.log("SUCCESS", response);
         },
         function(error) {
-                console.log("FAILED", error);
+            console.log("FAILED", error);
         });
-        return false;  // To block from loading a new page
-    }  
-
-
-
-
-
-
-
-
+    return false; // To block from loading a new page
+}
